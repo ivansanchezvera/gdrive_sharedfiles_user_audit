53e35a931155246f9a9a4dfcb2a4a05d177ffa8e
Ivan Sanchez <ivansanchez_1988@hotmail.com>
2023-01-30 16:02:18 -0500

    Improvements to the CLI to be more user-friendly

--- cli.py
+++ cli.py
@@ -7,20 +7,22 @@ def googleDriveAuditCLI():
     print("***GDrive Audit file Activities***")
     print("First We'll need some arguments from you to get the Activities from your Google Drive")
     print("Let's start with an initial date (earliest date) to start the filtering of GDrive Activities.")
+    print()
     print("Please insert the initial date in the ddmmyyyy format (ex 22112010): ")
     startDate = input()
-    formatedStartDate = datetime.datetime.strptime(startDate, "%d%m%Y").date()
+    formatedStartDate = datetime.datetime.strptime(startDate, "%d%m%Y") #.date()
     print(f"Great, initial date is {formatedStartDate}")
     print()
     print("Nice, now please provide the end date in the ddmmyyyy format (ex 22112010) to filter the GDrive Activity: ")
     endDate = input()
-    formatedEndDate = datetime.datetime.strptime(endDate, "%d%m%Y").date()
-    print(f"Great, initial date is {formatedEndDate}")
+    formatedEndDate = datetime.datetime.strptime(endDate, "%d%m%Y") #.date()
+    print(f"Ok, End date is {formatedEndDate}")
     if(formatedStartDate>formatedEndDate):
-        print("Sorry your End Date is earlier than the Start date")
+        print("Sorry your End Date is earlier than the Start date", file=sys.stderr)
+        print("\n\n\n")
         googleDriveAuditCLI()
     else:
-        main()
+        main(formatedStartDate, formatedEndDate)
 
 
 def CLImain():
