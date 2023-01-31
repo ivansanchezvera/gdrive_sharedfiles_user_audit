45ac3429ac1b5b2c3f8efafd76290cd1d1ce88f7
Ivan Sanchez <ivansanchez_1988@hotmail.com>
2023-01-31 15:07:26 -0500

    Cleanup: Erased commented out code.

--- main.py
+++ main.py
@@ -145,60 +145,9 @@ def consolidateGDriveInformation(
             print("********END OF THIS FILE********")
             print("\n")
 
-    # #Now the drive Activity code.
-    # service = build('driveactivity', 'v2', credentials=creds)
-
-    # # Call the Drive Activity API
-    # try:
-    #     results = service.activity().query(body={
-    #         'pageSize': 3,
-    #         # "filter": "time <= \"2015-06-01T00:00:00-05:00\" AND actor != """,
-    #         "filter": "time <= \"2015-06-01T00:00:00-05:00\"",
-    #     }).execute()
-    #     activities = results.get('activities', [])
-
-    #     if not activities:
-    #         print('No activity.')
-    #     else:
-    #         print('Recent activity:')
-    #         for activity in activities:
-    #             time = getTimeInfo(activity)
-    #             action = getActionInfo(activity['primaryActionDetail'])
-    #             actors = map(getActorInfo, activity['actors'])
-    #             targets = map(getTargetInfo, activity['targets'])
-    #             actors_str, targets_str = "", ""
-    #             actor_name = actors_str.join(actors)
-    #             target_name = targets_str.join(targets)
-
-    #             # Print the action occurred on drive with actor, target item and timestamp
-    #             print(u'{0}: {1}, {2}, {3}'.format(time, action, actor_name, target_name))
-
-    # except HttpError as error:
-    #     # TODO(developer) - Handleerrors from drive activity API.
-    #     print(f'An error occurred: {error}')
-
 
 def google_auth_get_credentials():
     creds = None
-    # # The file token.json stores the user's access and refresh tokens, and is
-    # # created automatically when the authorization flow completes for the first
-    # # time.
-    # if os.path.exists('credentials.json'):
-    #     # creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    #     creds = Credentials.from_authorized_user_file('credentials.json', SCOPES)
-
-    # # If there are no (valid) credentials available, let the user log in.
-    # if not creds or not creds.valid:
-    #     if creds and creds.expired and creds.refresh_token:
-    #         creds.refresh(Request())
-    #     else:
-    #         flow = InstalledAppFlow.from_client_secrets_file(
-    #             'credentials.json', SCOPES)
-    #         creds = flow.run_local_server(port=0)
-    #     # Save the credentials for the next run
-    #     with open('token.json', 'w') as token:
-    #         token.write(creds.to_json())
-
 
 	# Check if file token.pickle exists
     if os.path.exists('token.pickle'):
